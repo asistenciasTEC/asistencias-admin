635f96121de97dba7f233f63aa660c238724af9f
JeanCUS <49496475+JeanCUS@users.noreply.github.com>
2023-05-03 00:03:58 -0600

    update Asistencias and Styles
    - Se agrega la funcionalidad de exportar a Excel.
    - Se añaden estilos para este funcionalidad.

--- src/components/Asistencias.jsx
+++ src/components/Asistencias.jsx
@@ -1,14 +1,21 @@
 import { useState, useEffect } from "react";
-import {collection, query, where, getDocs, updateDoc} from "firebase/firestore";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
 import { Table, Modal, Form, Button } from "react-bootstrap";
 import { db } from "../config/firebase/firebase";
+import ReactHTMLTableToExcel from "react-html-table-to-excel";
 
 //librería de mensajes información
 import { toast, ToastContainer } from "react-toastify";
 
 //librería de iconos boostrap para react
-import {MdEdit} from "react-icons/md";
-
+import { MdEdit } from "react-icons/md";
+import {AiOutlineDownload} from "react-icons/ai"
 function Asistencias() {
   const [asistencias, setAsistencias] = useState([].sort());
   const [dataForm, setDataForm] = useState({
@@ -23,7 +30,14 @@ function Asistencias() {
   const [modalTitle, setModalTitle] = useState("");
   const [modalAction, setModalAction] = useState("");
 
-  const { id, tipoAsistencia, promedioPonderado, notaCurso, semestresActivos, creditosAprobados } = dataForm;
+  const {
+    id,
+    tipoAsistencia,
+    promedioPonderado,
+    notaCurso,
+    semestresActivos,
+    creditosAprobados,
+  } = dataForm;
   const handleChange = (e) => {
     setDataForm({
       ...dataForm,
@@ -44,17 +58,17 @@ function Asistencias() {
   }, []);
 
   const abrirModal = (accion, id = "") => {
-      const asistencia = asistencias.find((asistencia) => asistencia.id === id);
-      setModalTitle("Editar requisitos");
-      setModalAction("Guardar cambios");
-      setDataForm({
-        id: asistencia.id,
-        tipoAsistencia: asistencia.tipoAsistencia,
-        promedioPonderado: asistencia.promedioPonderado,
-        notaCurso: asistencia.notaCurso,
-        semestresActivos: asistencia.semestresActivos,
-        creditosAprobados: asistencia.creditosAprobados,
-      });
+    const asistencia = asistencias.find((asistencia) => asistencia.id === id);
+    setModalTitle("Editar requisitos");
+    setModalAction("Guardar cambios");
+    setDataForm({
+      id: asistencia.id,
+      tipoAsistencia: asistencia.tipoAsistencia,
+      promedioPonderado: asistencia.promedioPonderado,
+      notaCurso: asistencia.notaCurso,
+      semestresActivos: asistencia.semestresActivos,
+      creditosAprobados: asistencia.creditosAprobados,
+    });
     setShowModal(true);
   };
 
@@ -64,7 +78,13 @@ function Asistencias() {
 
   const editarAsistencia = async (e) => {
     e.preventDefault();
-    const asistenciaActualizado = { tipoAsistencia, promedioPonderado, notaCurso, semestresActivos, creditosAprobados };
+    const asistenciaActualizado = {
+      tipoAsistencia,
+      promedioPonderado,
+      notaCurso,
+      semestresActivos,
+      creditosAprobados,
+    };
     const q = query(collection(db, "asistencias"), where("id", "==", id));
     const querySnapshot = await getDocs(q);
 
@@ -86,9 +106,22 @@ function Asistencias() {
 
   return (
     <div className="container-lg ">
-      <h1>Asistencias</h1>
-      <Table striped bordered hover>
-        <thead className="table-dark table-bg-scale-50">
+      <div className="containerToTitleAndExportToExcel">
+        <h1>Asistencias</h1>
+        <div align="center">
+          <ReactHTMLTableToExcel
+            id="botonExportarExcel"
+            className="btn btn-success"
+            table="infoAsistencias"
+            filename="infoAsistencias"
+            sheet="pagAsistencias"
+            buttonText="Exportar a Excel"
+          />
+        </div>
+      </div>
+
+      <Table striped bordered hover id="infoAsistencias">
+        <thead className="table-dark table-bg-scale-50 headerToExcel">
           <tr>
             <th>Tipo de asistencia</th>
             <th>Promedio Ponderado</th>
@@ -112,20 +145,20 @@ function Asistencias() {
                   variant="warning"
                   onClick={() => abrirModal("editar", asistencia.id)}
                 >
-                  <MdEdit/>
+                  <MdEdit />
                 </Button>
               </td>
             </tr>
           ))}
         </tbody>
       </Table>
+
       <Modal show={showModal} onHide={cerrarModal}>
         <Modal.Header closeButton>
           <Modal.Title>{modalTitle}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form id="form1" onSubmit={editarAsistencia}>
-            
             <Form.Group className="mb-3" controlId="tipoAsistencia">
               <Form.Label>Tipo de asistencia</Form.Label>
               <Form.Control
@@ -184,7 +217,6 @@ function Asistencias() {
                 required
               />
             </Form.Group>
-
           </Form>
         </Modal.Body>
         <Modal.Footer>
