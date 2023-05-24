eac78fe285576ca5fa34542108cd17f02bb47435
JeanCUS <49496475+JeanCUS@users.noreply.github.com>
2023-05-24 13:09:43 -0600

    ultimsum

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,18 +2,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footercito">
-      <p className="title copyR">Desarrolladores</p>
-      <div className="containerF ">
-        <div className="containerDevelopers">
-          <p>Harold Enrique Ramírez Mora</p>
-          <p>Daniel Vargas Castro</p>
-          <p>Armando Lorenzo Villalta Pérez</p>
-          <p>Jean Carlos Urbina Silva</p>
-        </div>
+    <footer className="footer copyR">
+      <div className="containerD ">
+        <p>Harold Enrique Ramírez Mora</p>
+        <p>Daniel Vargas Castro</p>
+        <p>Armando Lorenzo Villalta Pérez</p>
+        <p>Jean Carlos Urbina Silva</p>
       </div>
-      &copy; 2023 SGA-TEC Company. Todos los derechos reservados.
-      {/* <p className="copyR"></p> */}
     </footer>
   );
 };
